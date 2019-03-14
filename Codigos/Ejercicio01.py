5d086ab17fcfb0881e88a27f1f657745e40f1ed5
316141952 <valeria.ccervantes@ciencias.unam.mx>
2019-03-14 00:58:49 -0600

    Ya acabé las preguntitas e hice el primer ejercicio de los programas

---
+++ Codigos/Ejercicio01.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''Aquí está el Ejercicio 1'''
+
+import Ejercicio01F 
+from Ejercicio01F import rect
+
+a = input ('Dame la altura del rectángulo: ')
+b = input ('Dame el ancho del rectángulo: ')
+print 'El rectángulo es: '
+print rect(a,b)
---
+++ Codigos/Ejercicio01F.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'Funciones del ejercicio 1'
+
+def rect (a,b) :
+    '''Dando una base y una altura,
+te devuelve un rectángulo con *'''
+    L = range(b)
+    j = 0
+    while j < a :
+        for i in range(len(L)) :
+            print '*',
+        j = j + 1
+        print '\n'
+
+
+t = rect (2,3)
+print t
