432d0bfaf0a091b317ab444f3e022873246a9811
316141952 <valeria.ccervantes@ciencias.unam.mx>
2019-03-17 11:49:01 -0600

    Los ejercicios 1 y 2

---
+++ Codigos/Ejercicio02.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#Ejercicio 2
+
+import Ejercicio02F
+from Ejercicio02F import *
+
+n = input ("Dame la altura del triángulo: ")
+a = input ("Dame la base del triángulo: ")
+print "El triángulo es:"
+triangulo (n)
+trian_inv (n)
--- Codigos/Ejercicio02F.py
+++ Codigos/Ejercicio02F.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 #Función del ejercicio 2
 
-def t (a,b):
-#a es la altura y b es la base
-    '''Dando una base y una altura,
-te devuelve un triángulo con *'''
+def orden_l (x) :
+    '''Ordena una lista de mayor a menor'''
+    for i in range(len(x)-1,0,-1): #te da una lista volteada con los índices de la entrada
+        for j in range(i) :
+            if x[j]<x[j+1]: #aquí compara los elementos uno por uno
+                t = x[j]    #y esto los intercambia si uno es mayor que el otro
+                x[j] = x[j+1] 
+                x[j+1] = t
+    return (x) 
 
-    
 def triangulo(n):
     for x in range (1,n+1):
+        print x*"*"
+
+def trian_inv (n):
+    L = range (1,n)
+    L1 = orden_l (L)
+    for x in L1 :
         print x*"*"
